44a282fab21d2c451d02ee5d0448e5c270bb66d7
Santi <sgrautorredemer@apple.com>
2015-01-23 11:35:45 -0800

    basic stuff

---
+++ main.js
@@ -0,0 +1,50 @@
+// ┌────────────────────────────────────────────────────────────────────┐
+// | Filename: main.js
+// └────────────────────────────────────────────────────────────────────┘
+
+// ┌────────────────────────────────────────────────────────────────────┐
+// | Require modules
+// └────────────────────────────────────────────────────────────────────┘
+var express = require("express");
+var stylus = require('stylus');
+var nib = require('nib');
+var fs = require('fs');
+var figlet = require('figlet');
+
+// ┌────────────────────────────────────────────────────────────────────┐
+// | Initialize vars + constants
+// └────────────────────────────────────────────────────────────────────┘
+var app = express();
+var port = Number(process.env.PORT || 5000);
+
+// ┌────────────────────────────────────────────────────────────────────┐
+// | App setup
+// └────────────────────────────────────────────────────────────────────┘
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'jade');
+app.use(
+	stylus.middleware({
+		src: __dirname + '/public',
+		compile: function(str, path) { return stylus(str).set('filename', path).use(nib()); }
+	})
+);
+app.use(express.static(__dirname + '/public'));
+
+// ┌────────────────────────────────────────────────────────────────────┐
+// | Routes
+// └────────────────────────────────────────────────────────────────────┘
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+// ┌────────────────────────────────────────────────────────────────────┐
+// | Init!!
+// └────────────────────────────────────────────────────────────────────┘
+app.listen(port, function() {
+	console.log('\n↳ Listening on port: ' + port);
+});
+
+
+figlet('Hello World!!', function(err, data) {
+	console.log(data)
+});
