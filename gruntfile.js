cd1590f9db74c1a0f8e6178d427adac6fc5e2a5d
Santi Grau <sgrautorredemer@apple.com>
2015-02-17 16:50:47 -0800

    add grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,21 +1,76 @@
 module.exports = function(grunt) {
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    requirejs: {
+
+    clean: {
+      build: {
+        files: [{
+          dot: true,
+          src: [
+            'build/*'
+          ]
+        }]
+      }
+    },
+
+    copy: {
+      build: {
+        files: [
+          {
+            expand: true,
+            dot: true,
+            cwd: 'public',
+            src: [
+              'assets/**/*',
+              'css/**/*.css',
+              'js/**/*.js'
+            ],
+            dest: 'build',
+          },
+        ],
+      },
+    },
+
+    stylus: {
       compile: {
         options: {
-          almond: true,
-          modules: [{name: 'main'}],
-          dir: './public/build',
-          baseUrl : "./public/js",
-          mainConfigFile: "./public/js/main.js"
+          paths: ['public/css/main.styl'],
+          use: [
+            require('nib') // use stylus plugin at compile time
+          ]
+        },
+        files: {
+          'build/css/main.css': 'public/css/main.styl',
+        }
+      }
+    },
+
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false
+          }
+        },
+        files: {
+          'build/index.html': ['views/index.jade']
         }
       }
     }
   });
+
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-requirejs');
-  // Default task(s).
-  grunt.registerTask('default', ['requirejs']);
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Build Tasks
+  grunt.registerTask('build', [
+    'clean',
+    'copy',
+    'stylus',
+    'jade',
+  ]);
 };
