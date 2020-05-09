fdd2cbf5598ab6c677aa895ad929532b3c54b915
santi <santi@proper-code.com>
2020-05-09 12:20:44 -0700

    update lol

---
+++ app/js/main.js
@@ -0,0 +1,3 @@
+console.log('Welcome')
+document.body.style.background = '#'+((1<<24)*Math.random()|0).toString(16)
+document.getElementById('main').style.color = '#'+((1<<24)*Math.random()|0).toString(16)
